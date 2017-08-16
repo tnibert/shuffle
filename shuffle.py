25b732a4e2e2c3f046920b5e8b9299dd7ff2fbaa
Tim Nibert <docz2a@gmail.com>
2017-08-16 22:56:12 +1000

    plays one random song

--- shuffle.py
+++ shuffle.py
@@ -1,19 +1,23 @@
 #! /usr/bin/env python
 from subprocess import Popen, PIPE
 import sys
+from random import randint
 
 dirlisting = []
 
 proc = Popen(['ls *.mp3'], shell=True, stdout=PIPE)
 for line in proc.stdout.readlines():
     if line != '':
-        print line
+        #print(line)
         dirlisting.append(line)
 
 
-print dirlisting[0]
+#print(dirlisting[0])
 
 pipes = dict(stdin=PIPE, stdout=PIPE, stderr=PIPE)
-for song in dirlisting:
-    mplayer = Popen(["mplayer", song.rstrip()], **pipes)
-    mplayer.communicate(input=b">")
+
+song = dirlisting[randint(0, len(dirlisting))]
+print(song)
+
+mplayer = Popen(["mplayer", song.rstrip()], **pipes)
+mplayer.communicate(input=b">")
