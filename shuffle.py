1918591c301cbb3d4dd99ee9c694eb58bf1970f6
Tim Nibert <docz2a@gmail.com>
2017-08-16 23:03:46 +1000

    put on loop

--- shuffle.py
+++ shuffle.py
@@ -17,7 +17,8 @@ for line in proc.stdout.readlines():
 pipes = dict(stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
 song = dirlisting[randint(0, len(dirlisting))]
-print(song)
 
-mplayer = Popen(["mplayer", song.rstrip()], **pipes)
-mplayer.communicate(input=b">")
+while True:
+    print(song)
+    mplayer = Popen(["mplayer", song.rstrip()], **pipes)
+    mplayer.communicate(input=b">")
