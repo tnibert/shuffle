c2136ccaf5431d05e896ad38552769d6c8b194ae
Tim Nibert <docz2a@gmail.com>
2017-10-15 13:14:25 +1100

    figuring out realtime keyboard input

--- shuffle.py
+++ shuffle.py
@@ -4,10 +4,24 @@ import sys
 from random import randint
 import vlc
 import time
+import termios, fcntl, os
+
+# set up keyboard input
+fd = sys.stdin.fileno()
+
+oldterm = termios.tcgetattr(fd)
+newattr = termios.tcgetattr(fd)
+newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+# get all mp3s in our current directory
 
 dirlisting = []
 
-proc = Popen(['ls *.mp3'], shell=True, stdout=PIPE)
+proc = Popen(['ls *.mp3'], shell=True, stdout=PIPE)         # *nix only, if you don't like it, change it ;)
 for line in proc.stdout.readlines():
     if line != '':
         #print(line)
@@ -26,4 +40,8 @@ while True:
     player = vlc.MediaPlayer(song.rstrip())
     player.play()
     time.sleep(2)
-    while player.is_playing(): time.sleep(1)
+    while player.is_playing():
+        try:
+            c = sys.stdin.read(1)
+            print "Got character", repr(c)
+        except IOError: pass
