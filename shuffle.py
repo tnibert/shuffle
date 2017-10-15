076c32de4a185a5211e4a947c1a0775a25491854
Tim Nibert <docz2a@gmail.com>
2017-10-15 13:04:16 +1100

    now implementing playback via vlc bindings

--- shuffle.py
+++ shuffle.py
@@ -2,6 +2,8 @@
 from subprocess import Popen, PIPE
 import sys
 from random import randint
+import vlc
+import time
 
 dirlisting = []
 
@@ -19,5 +21,9 @@ pipes = dict(stdin=PIPE, stdout=PIPE, stderr=PIPE)
 while True:
     song = dirlisting[randint(0, len(dirlisting)-1)]
     print(song)
-    mplayer = Popen(["mplayer", song.rstrip()], **pipes)
-    mplayer.communicate(input=b">")
+    #mplayer = Popen(["mplayer", song.rstrip()], **pipes)
+    #mplayer.communicate(input=b">")
+    player = vlc.MediaPlayer(song.rstrip())
+    player.play()
+    time.sleep(2)
+    while player.is_playing(): time.sleep(1)
